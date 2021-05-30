2bd6c0bcd0b4b90608012816b009a5ab3fb3a860
Martin Bonnin <martin@mbonnin.net>
2021-05-30 21:38:24 +0200

    use Cloud Run instead of App Engine

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   kotlin("jvm").version("1.5.10")
   kotlin("plugin.spring").version("1.5.10")
-  id("org.springframework.boot").version("2.5.0")
-  id("com.google.cloud.tools.appengine").version("2.4.1")
+  id("org.springframework.boot").version("2.4.2")
+  id("com.bmuschko.docker-spring-boot-application") version "7.0.0"
 }
 
 dependencies {
@@ -10,13 +10,6 @@ dependencies {
   implementation("org.mindrot:jbcrypt:0.4")
   implementation("com.google.cloud:google-cloud-firestore:1.32.0")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.5.10")
-}
-
-appengine {
-  deploy {
-    projectId = "fraug-votes"
-    version = "GCLOUD_CONFIG"
-  }
 }
 
 tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
@@ -29,15 +22,16 @@ tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).conf
  * Deploy the executable jar by default
  * See https://docs.spring.io/spring-boot/docs/2.5.0/gradle-plugin/reference/htmlsingle/#packaging-executable.and-plain-archives
  */
-tasks.getByName<Jar>("jar") {
-  enabled = false
-}
+//tasks.getByName<Jar>("jar") {
+//  enabled = false
+//}
 
-appengine {
-  this.stage {
-    this.setArtifact(tasks.named("bootJar").flatMap {
-      it as Jar
-      it.archiveFile
-    })
+docker {
+  springBootApplication {
+    // openjdk:8-alpine is not enough because of a TLS thingie
+    // See https://github.com/grpc/grpc-java/issues/5369#issuecomment-493463266
+    baseImage.set("openjdk:8")
+    ports.set(listOf(8080))
+    jvmArgs.set(listOf("-Dspring.profiles.active=production", "-Xmx52m"))
   }
 }
