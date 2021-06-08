c0a16327f76722ea69c8ac1c6a0c5c04385e3e68
Martin Bonnin <martin@mbonnin.net>
2021-06-08 23:42:20 +0200

    update

--- server/src/main/kotlin/fraug/votes/backend/model/Firestore.kt
+++ server/src/main/kotlin/fraug/votes/backend/model/Firestore.kt
@@ -1,13 +1,8 @@
 package fraug.votes.backend.model
 
 import com.google.auth.oauth2.GoogleCredentials
-import com.google.cloud.firestore.DocumentReference
 import com.google.cloud.firestore.FirestoreOptions
 import com.google.cloud.firestore.Transaction
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.channels.sendBlocking
-import kotlinx.coroutines.flow.callbackFlow
 
 
 object Firestore {
@@ -17,93 +12,25 @@ object Firestore {
         .build();
     private val firestoreDb = firestoreOptions.service
 
-    fun getPasswordHash(): String? {
-        val documentRef = firestoreDb.document("settings/admin")
-        return documentRef.get().get().getString("password")
-    }
-
-    fun setPasswordHash(passwordHash: String) {
-        val documentRef = firestoreDb.document("settings/admin")
-        documentRef.set(mapOf("password" to passwordHash)).get()
-    }
-
-    fun deletePasswordHash() {
-        val documentRef = firestoreDb.document("settings/admin")
-        documentRef.delete()
-    }
-
-    fun createQuestion(questionId: String, choiceIds: List<String>) = withTransaction {
-        val documentRef = firestoreDb.document("question/$questionId")
-
-        val existingChoiceIds = it.get(documentRef).get().get("choiceIds")
-        check(existingChoiceIds == null) {
-            "There is already a question with id '$questionId'"
-        }
-        it.set(documentRef, mapOf("choiceIds" to choiceIds))
-    }
-
-    fun getQuestionChoices(questionId: String): List<String>? {
-        val documentRef = firestoreDb.document("question/$questionId")
-
-        return documentRef.get().get().get("choiceIds") as List<String>?
-    }
-
-    fun getQuestions(): List<FirestoreQuestion> {
-        return firestoreDb.collection("question").listDocuments()
-          .map {
-              FirestoreQuestion(it.id, getQuestionChoices(it.id) ?: emptyList())
-          }
-    }
-
-    fun deleteQuestion(questionId: String) {
-        val documentRef = firestoreDb.document("question/$questionId")
-        deleteDocument(documentRef)
-    }
-
-    fun setActiveQuestion(questionId: String?) {
-        val documentRef = firestoreDb.document("settings/admin")
-        documentRef.update(mapOf("activeQuestion" to questionId))
-    }
-
     fun upsertVote(userId: String, choiceId: String) = withTransaction {
-        val adminDocumentRef = firestoreDb.document("settings/admin")
-        val questionId = it.get(adminDocumentRef).get().get("activeQuestion")
-        check (questionId != null) {
+        val currentGame = it.get(firestoreDb.document("settings/admin")).get().get("currentGame")
+        check (currentGame != null) {
+            "No active currentGame"
+        }
+        val currentQuestion = it.get(firestoreDb.document("games/$currentGame")).get().get("currentQuestion")
+        check (currentQuestion != null) {
             "No active question"
         }
-        val questionDocumentRef = firestoreDb.document("question/$questionId/vote/$userId")
-        it.set(questionDocumentRef, mapOf("choiceId" to choiceId))
-    }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    fun listenToVotes(questionId: String) = callbackFlow {
-        val registration = firestoreDb.collection("question/$questionId/vote").addSnapshotListener { querySnapshot, error ->
-            if (querySnapshot != null) {
-                val choices = querySnapshot.documents.map {
-                    it.getString("choiceId")
-                }
-
-                kotlin.runCatching {
-                    sendBlocking(choices)
-                }
-            }
+        val questionDocumentReference = firestoreDb.document("games/$currentGame/questions/$currentQuestion")
+        check (it.get(questionDocumentReference).get().get("voteOpened") == true) {
+            "Votes are not open"
         }
 
-        awaitClose {
-            registration.remove()
-        }
-    }
-
-    private fun deleteDocument(documentRef: DocumentReference) {
-        documentRef.listCollections().forEach {
-            it.listDocuments().forEach {
-                deleteDocument(it)
-            }
-        }
-        documentRef.delete().get()
+        val votesDocumentReference = firestoreDb.document("games/$currentGame/questions/$currentQuestion/votes/$userId")
+        it.set(votesDocumentReference, mapOf("vote" to choiceId))
     }
 
     private fun withTransaction(block: (Transaction) -> Unit)  = firestoreDb.runTransaction { block(it) }.get()
 }
 
-class FirestoreQuestion(val id: String, val choiceIds: List<String>)
--- server/src/main/kotlin/fraug/votes/backend/model/operations.kt
+++ server/src/main/kotlin/fraug/votes/backend/model/operations.kt
@@ -16,40 +16,18 @@ import org.springframework.stereotype.Component
 
 @Component
 class RootQuery : Query {
-  fun questions(): List<Question> {
-    return Firestore.getQuestions().map {
-      Question(it.id, it.choiceIds)
-    }
+  fun hello(): String {
+    return "\uD83D\uDC38"
   }
 }
 
 @Component
 class RootMutation : Mutation {
-  fun setup(password: String): Boolean {
-    val existingPasswordHash = Firestore.getPasswordHash()
-    check(existingPasswordHash == null) {
-      "This instance has already been set up"
-    }
-
-    val hash = BCrypt.hashpw(password, BCrypt.gensalt())
-    Firestore.setPasswordHash(hash)
-
-    return true
-  }
-
-  fun admin(password: String): Admin {
-    check(BCrypt.checkpw(password, Firestore.getPasswordHash())) {
-      "wrong password"
-    }
-
-    return Admin()
-  }
-
   /**
    * register a vote for the active question
    *
    * @param userId an identifier for a user. A given user can change their votes by voting multiple times
-   * @param choiceId a choiceId. No validation is made. If an invalide choiceId is entered, the vote will
+   * @param choiceId a choiceId. No validation is made. If an invalid choiceId is entered, the vote will
    * be ignored
    *
    * @return always returns true. Failures will be returned as errors
@@ -57,83 +35,5 @@ class RootMutation : Mutation {
   fun vote(userId: String, choiceId: String): Boolean {
     Firestore.upsertVote(userId, choiceId)
     return true
-  }
-}
-
-
-@Component
-class RootSubscription : Subscription {
-  @OptIn(FlowPreview::class)
-  @GraphQLDescription("Listens to votes for a given question")
-  fun votes(questionId: String): Publisher<List<VoteCount>> {
-    val choiceIds = Firestore.getQuestionChoices(questionId)
-
-    check(choiceIds != null)
-
-    return Firestore.listenToVotes(questionId).map { votes ->
-      choiceIds.map { choiceId ->
-        VoteCount(choiceId, votes.count { vote -> vote == choiceId })
-      } + VoteCount(null, votes.count { vote -> !choiceIds.contains(vote) })
-    }.debounce(100)
-      .asPublisher()
-  }
-}
-
-@GraphQLDescription("The root for mutation that requires the admin password.")
-class Admin {
-  @GraphQLDescription("Deletes the admin password. Always true.")
-  fun deletePassword(): Boolean {
-    Firestore.deletePasswordHash()
-    return true
-  }
-
-  @GraphQLDescription("Create a new question. Always true.")
-  fun createQuestion(
-    @GraphQLDescription("a unique id for this question")
-    questionId: String,
-    @GraphQLDescription(" a list of choices. For a given question, all choices must be different. This is typically the identifier that users will input in the Twitch chat with `!benjamin`")
-    choiceIds: List<String>
-  ): Boolean {
-    check(choiceIds.size >= 2) {
-      "A question must have at least two choiceIds"
-    }
-    Firestore.createQuestion(questionId, choiceIds)
-    return true
-  }
-
-  @GraphQLDescription("Activate the given question. Votes will use this question from now on")
-  fun activateQuestion(questionId: String?): Boolean {
-    Firestore.setActiveQuestion(questionId)
-    return true
-  }
-
-  /**
-   *
-   *
-   * @return always returns true. Failures will be returned as errors
-   */
-  @GraphQLDescription("Delete the given question. Always true.")
-  fun deleteQuestion(questionId: String): Boolean {
-    Firestore.deleteQuestion(questionId)
-    return true
-  }
-}
-
-@GraphQLDescription("The number of votes for a given choice.")
-class VoteCount(
-  @GraphQLDescription("The choiceId. null for votes with an invalid choiceId.")
-  val choiceId: String?,
-  val count: Int
-)
-
-class Question(val id: String, val choiceIds: List<String>) {
-  fun votes(): List<VoteCount> {
-    return runBlocking {
-      Firestore.listenToVotes(id).first().let { votes ->
-        choiceIds.map { choiceId ->
-          VoteCount(choiceId, votes.count { vote -> vote == choiceId })
-        } + VoteCount(null, votes.count { vote -> !choiceIds.contains(vote) })
-      }
-    }
   }
 }
